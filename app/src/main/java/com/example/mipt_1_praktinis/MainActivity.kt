0af4ef9d091e66577cf7c1c60c63a7539eb9fe63
Modestas <modestas200402@gmail.com>
2024-09-14 21:15:25 +0300

    Text, buttons added.

--- app/src/main/java/com/example/mipt_1_praktinis/MainActivity.kt
+++ app/src/main/java/com/example/mipt_1_praktinis/MainActivity.kt
@@ -4,23 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.mipt_1_praktinis.ui.theme.CustomTheme
+import kotlin.random.Random
+
+val RANDOM_WORDS = arrayOf(
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+    "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yam", "zucchini"
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,11 +64,47 @@ class MainActivity : ComponentActivity() {
 fun CustomBanner(
     innerPadding: PaddingValues
 ) {
-    Column(
-        modifier = Modifier.padding(innerPadding)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 32.dp)
+                .widthIn(max = 350.dp)
+        ) {
+            val size = RANDOM_WORDS.size
+            var index by remember { mutableStateOf(Random.nextInt(0, size)) }
 
+            CustomText(RANDOM_WORDS[index])
+            Spacer(modifier = Modifier.height(32.dp))
+            CustomButton(
+                text = "Change text",
+                imageVector = Icons.Rounded.Refresh,
+                onClick = { index = Random.nextInt(0, size) }
+            )
+        }
     }
+}
+
+@Composable
+fun CustomText(
+    text: String,
+) {
+    Text(
+        text = text,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(horizontal = 16.dp, vertical = 16.dp)
+            .fillMaxWidth()
+
+    )
 }
 
 @Composable
@@ -54,7 +114,13 @@ fun CustomButton(
     onClick: () -> Unit,
     spacerWidth: Dp = 8.dp
 ) {
-    OutlinedButton(onClick = onClick) {
+    OutlinedButton(
+        onClick = onClick,
+        contentPadding = PaddingValues(
+            horizontal = 24.dp,
+            vertical = 16.dp
+        ),
+    ) {
         Icon(imageVector = imageVector, contentDescription = null)
         Spacer(modifier = Modifier.width(spacerWidth))
         Text(text)
@@ -68,3 +134,4 @@ fun CustomBannerPreview() {
         CustomBanner(PaddingValues())
     }
 }
+
